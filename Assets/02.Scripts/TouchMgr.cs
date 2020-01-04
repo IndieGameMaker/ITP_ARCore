cf578e442c4056a2bbbc58fb9ab24fd0ca18ae5e
Jaehyun Lee <myleje@gmail.com>
2020-01-04 16:36:05 +0900

    .

---
+++ Assets/02.Scripts/TouchMgr.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchMgr : MonoBehaviour
+{
+    private Camera arCamera;
+    private GameObject monster;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        arCamera = Camera.main;
+        monster = Resources.Load<GameObject>("Monster");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
